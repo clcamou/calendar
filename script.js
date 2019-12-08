f7f18273bcf35a77950a3307782a3312ecb5d8f1
clcamou <clcamou@gmail.com>
2019-12-07 18:16:31 -0700

    restyled CSS

--- script.js
+++ script.js
@@ -1,81 +1,188 @@
-let today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
-let selectYear = document.getElementById("year");
-let selectMonth = document.getElementById("month");
-
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-let monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
-
-
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
-}
-
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
-function showCalendar(month, year) {
-
-    let firstDay = (new Date(year, month)).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    let tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-
-    // creating all cells
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }
-            else if (date > daysInMonth) {
-                break;
-            }
-
-            else {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
-                } // color today's date
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                date++;
-            }
-
-
-        }
-
-        tbl.appendChild(row); // appending each row into calendar body.
+$(document).ready(function() {
+  
+    // test flag
+    let test = false;
+  
+    // get times from moment
+    let now = moment().format('MMMM Do YYYY');
+  
+    // commented out for test in non-standard hours
+    let nowHour24 = moment().format('H');
+    let nowHour12 = moment().format('h');
+  
+    // set times for tesitng after hours
+    if (test) {
+      nowHour24 = 13;
+      nowHour12 = 1;
     }
-
-}
+  
+    let $dateHeading = $('#navbar-subtitle');
+    $dateHeading.text(now);
+    
+    // using font awesome icon https://fontawesome.com/license
+    // change description here - none
+    let saveIcon = "./images/save-regular.svg"; 
+  
+    // Get stored todos from localStorage
+    // Parsing the JSON string to an object
+    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+  
+    if (test) { console.log(storedPlans); }
+  
+    // If plans were retrieved from localStorage, update the plan array to it
+    if (storedPlans !== null) {
+      planTextArr = storedPlans;
+    } else {
+      // this should only occur on first time the app is loaded in the browser
+      // helpfully remind user that lunch is important
+      planTextArr = new Array(9);
+      planTextArr[4] = "Picnic lunch outside";
+    }
+  
+    if (test) { console.log("full array of plned text",planTextArr); }
+  
+    // set variable referencing planner element
+    let $plannerDiv = $('#plannerContainer');
+    // clear existing elements
+    $plannerDiv.empty();
+  
+    if (test) { console.log("current time",nowHour12); }
+  
+  
+    // build calendar by row for fix set of hours
+    for (let hour = 9; hour <= 17; hour++) {
+      // index for array use offset from hour
+      let index = hour - 9;
+      
+      // build row components
+      let $rowDiv = $('<div>');
+      $rowDiv.addClass('row');
+      $rowDiv.addClass('plannerRow');
+      $rowDiv.attr('hour-index',hour);
+    
+      // Start building Time box portion of row
+      let $col2TimeDiv = $('<div>');
+      $col2TimeDiv.addClass('col-md-2');
+    
+      // create timeBox element (contains time)
+      let $timeBoxSpn = $('<span>');
+      // can use this to get value
+      $timeBoxSpn.attr('class','timeBox');
+      
+      // format hours for display
+      let displayHour = 0;
+      let ampm = "";
+      if (hour > 12) { 
+        displayHour = hour - 12;
+        ampm = "pm";
+      } else {
+        displayHour = hour;
+        ampm = "am";
+      }
+      
+      // populate timeBox with time
+      $timeBoxSpn.text(`${displayHour} ${ampm}`);
+  
+      // insert into col inset into timebox
+      $rowDiv.append($col2TimeDiv);
+      $col2TimeDiv.append($timeBoxSpn);
+      // STOP building Time box portion of row
+  
+      // START building input portion of row
+      // build row components
+      let $dailyPlanSpn = $('<input>');
+  
+      $dailyPlanSpn.attr('id',`input-${index}`);
+      $dailyPlanSpn.attr('hour-index',index);
+      $dailyPlanSpn.attr('type','text');
+      $dailyPlanSpn.attr('class','dailyPlan');
+  
+      // access index from data array for hour 
+      $dailyPlanSpn.val( planTextArr[index] );
+      
+      // create col to control width
+      let $col9IptDiv = $('<div>');
+      $col9IptDiv.addClass('col-md-9');
+  
+      // add col width and row component to row
+      $rowDiv.append($col9IptDiv);
+      $col9IptDiv.append($dailyPlanSpn);
+      // STOP building Time box portion of row
+  
+      // START building save portion of row
+      let $col1SaveDiv = $('<div>');
+      $col1SaveDiv.addClass('col-md-1');
+  
+      let $saveBtn = $('<i>');
+      $saveBtn.attr('id',`saveid-${index}`);
+      $saveBtn.attr('save-id',index);
+      $saveBtn.attr('class',"far fa-save saveIcon");
+      
+      // add col width and row component to row
+      $rowDiv.append($col1SaveDiv);
+      $col1SaveDiv.append($saveBtn);
+      // STOP building save portion of row
+  
+      // set row color based on time
+      updateRowColor($rowDiv, hour);
+      
+      // add row to planner container
+      $plannerDiv.append($rowDiv);
+    };
+  
+    // function to update row color
+    function updateRowColor ($hourRow,hour) { 
+  
+      if (test) { console.log("rowColor ",nowHour24, hour); }
+  
+      if ( hour < nowHour24) {
+        // $hourRow.css('')
+        if (test) { console.log("lessThan"); }
+        $hourRow.css("background-color","lightgrey")
+      } else if ( hour > nowHour24) {
+        if (test) { console.log("greaterthan"); }
+        $hourRow.css("background-color","lightgreen")
+      } else {
+        if (test) { console.log("eqaul"); }
+        $hourRow.css("background-color","tomato")
+      }
+    };
+  
+    // saves to local storage
+    // conclick function to listen for user clicks on plan area
+    $(document).on('click','i', function(event) {
+      event.preventDefault();  
+  
+      if (test) { console.log('click pta before '+ planTextArr); }
+  
+      let $index = $(this).attr('save-id');
+  
+      let inputId = '#input-'+$index;
+      let $value = $(inputId).val();
+  
+      planTextArr[$index] = $value;
+  
+  
+      if (test) { console.log('value ', $value); }
+      if (test) { console.log('index ', $index); }
+      if (test) { console.log('click pta after '+ planTextArr); }
+  
+      // remove shawdow pulse class
+      $(`#saveid-${$index}`).removeClass('shadowPulse');
+      localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
+    });  
+    
+    // function to color save button on change of input
+    $(document).on('change','input', function(event) {
+      event.preventDefault();  
+      if (test) { console.log('onChange'); }
+      if (test) { console.log('id', $(this).attr('hour-index')); }
+  
+      // neeed to check for save button
+  
+      let i = $(this).attr('hour-index');
+  
+      // add shawdow pulse class
+      $(`#saveid-${i}`).addClass('shadowPulse');
+    });
+  });
